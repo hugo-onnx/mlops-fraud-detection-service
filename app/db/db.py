b537d1ac08179753f3a38034bf05822b8b0e7657
Hugo Gonzalez <hugonzalezhuerta@gmail.com>
2026-01-02 20:19:29 +0100

    fix: update db

--- app/db/db.py
+++ app/db/db.py
@@ -1,28 +1,53 @@
 import sqlite3
 import os
 from datetime import datetime, timezone
-from app.config.config import settings
+from app.config.config import settings, logger
+
 
 def init_db(feature_cols: list):
+    if not feature_cols:
+        raise RuntimeError("Feature columns list is empty — cannot initialize DB")
+
     os.makedirs(os.path.dirname(settings.DB_PATH), exist_ok=True)
+
+    cols_sql = ", ".join(f'"{c}" REAL' for c in feature_cols)
+
+    sql = f"""
+    CREATE TABLE IF NOT EXISTS "requests" (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        {cols_sql},
+        fraud_probability REAL,
+        timestamp TEXT
+    );
+    """
+
+    logger.info("Initializing SQLite DB")
+    logger.info(f"DB path: {settings.DB_PATH}")
+    logger.info(f"Create table SQL:\n{sql}")
+
     with sqlite3.connect(settings.DB_PATH) as conn:
         cursor = conn.cursor()
-        cols_sql = ", ".join([f'"{c}" REAL' for c in feature_cols])
-        cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS requests (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                {cols_sql},
-                fraud_probability REAL,
-                timestamp TEXT
-            );
-        """)
+        cursor.execute(sql)
         conn.commit()
+
 
 def log_request(features: dict, proba: float):
     with sqlite3.connect(settings.DB_PATH) as conn:
         cursor = conn.cursor()
-        columns = ", ".join([f'"{k}"' for k in features.keys()] + ["fraud_probability", "timestamp"])
+
+        columns = (
+            ", ".join(f'"{k}"' for k in features.keys())
+            + ', "fraud_probability", "timestamp"'
+        )
+
         placeholders = ", ".join(["?"] * (len(features) + 2))
-        values = list(features.values()) + [proba, datetime.now(timezone.utc).isoformat()]
-        cursor.execute(f"INSERT INTO requests ({columns}) VALUES ({placeholders})", values)
+        values = list(features.values()) + [
+            proba,
+            datetime.now(timezone.utc).isoformat(),
+        ]
+
+        cursor.execute(
+            f'INSERT INTO "requests" ({columns}) VALUES ({placeholders})',
+            values,
+        )
         conn.commit()
